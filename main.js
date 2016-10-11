4a97d45d1b760fcb85ca77946f8be01cc8757ef0
gavinleng <gleng.email@gmail.com>
2016-10-11 10:33:56 +0100

    v1.1

--- main.js
+++ main.js
@@ -11,20 +11,20 @@ var mappingId = require("./mappingId.js");
 //projection age group module
 var dataAgeGroup = require('./dataAgeGroup.js');
 
-//get dataConfig file
-var dataConfig = require('./dataConfig.js');
-
-var year1 = dataConfig.year;
-
-var stringArray = dataConfig.areaId;
-
 //var nqmPopPrjId = "B1xex1pXR";
 //var mappingId = "SkxyHi_MR";
-
-const outPath = path.resolve(dataConfig.outPath);
+/*var year = "2015;
+ var areaId = ["E06000045"];
+ var outPath = "./SotonAG2015.json";*/
 
 function databot(input, output, context) {
 	output.progress(0);
+	
+	var year1 = input.year;
+	
+	var stringArray = input.areaId;
+	
+	const outPath = path.resolve(input.outPath);
 	
 	var p2c_config = {"mappingId": input.mappingId, "parent_type": "LAD15CD", "child_type": "LSOA11CD", "dataId": stringArray, "context": context, "output": output};
 	
